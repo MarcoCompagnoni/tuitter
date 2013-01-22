4c42c56b9e9f57ec1eafebba554ee64330541834
insubria <insubria@b04.(none)>
2013-01-22 15:46:37 +0100

    Aggiunta scelta categorie

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,7 @@ class TweetsController < ApplicationController
   before_filter :authenticate, :only => :create
 
   def index
+  	@categories = Category.all
     @tweets = Tweet.includes("user").order("created_at desc")
     if current_user
       users_i_am_interested_in = [current_user] + current_user.users_that_i_follow
@@ -9,8 +10,12 @@ class TweetsController < ApplicationController
     end
   end
   
-  def create    
-		category = Category.find_or_create_by_name(params[:tweet][:category])
+  def create
+  	if params[:tweet][:category].empty?
+			category = Category.find_or_create_by_name(params[:tweet][:select_category])
+		else
+			category = Category.find_or_create_by_name(params[:tweet][:category])
+		end
     @tweet = current_user.tweets.build(
 		:text => params[:tweet][:text],
 		:user_id => params[:tweet][:user_id],
